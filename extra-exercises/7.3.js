1be744e36a375e7d4ad0f0c441a36e6549146c83
Raileanu Alexandru <alex.raileanu91@gmail.com>
2020-10-16 15:15:26 +0200

    extra exercises finished

--- extra-exercises/7.3.js
+++ extra-exercises/7.3.js
@@ -1,14 +1,19 @@
 const readlineSync = require("readline-sync");
 let n = new Number(readlineSync.question("Enter a positive integer n "));
 let arr = new Array;
-if(n >= 0){
-for(let i = 2; i <= n/2; i++){
-    if(n % i == 0){
-        arr.push(i);
+if (n >= 0) {
+    for (let i = 2; i <= n / 2; i++) {
+        if (n % i == 0) {
+            arr.push(i);
+        }
+    }
+    if (arr[0] == null) {
+        console.log(n + " is a prime number");
+    }
+    else {
+        console.log("divisors of " + n + " are: " + arr);
     }
 }
-console.log("divisors of " + n + " are: "+ arr);
-}
-else{
+else {
     console.log("It's not a positive integer")
 }
--- extra-exercises/7.4.js
+++ extra-exercises/7.4.js
@@ -1,8 +1,36 @@
 const readlineSync = require("readline-sync");
 var fs = require('fs');
 
-console.log("Hello! Welcome to the Pizza Flavors Manager.\n\n Please choose your actions:\n\n1 - List all the pizza flavors\n2 - Add a new pizza flavor\n3 - Remove a pizza flavor\n4 - Exit this program ");
+//let pizzaFlavors = fs.readFile('pizza-flavors.txt', 'utf8');
+let arr = new Array;
 
-let n = new Number(readlineSync.question("Enter your action's number: "));
+console.log("Hello! Welcome to the Pizza Flavors Manager.\n\n");
+
+while (true) {
+    console.log("Please choose your actions:\n\n1 - List all the pizza flavors\n2 - Add a new pizza flavor\n3 - Remove a pizza flavor\n4 - Exit this program \n\n");
+    var n = parseInt(readlineSync.question("Enter your action's number: \n"));
 
 
+    switch (n) {
+        case 1:
+            if (arr.length == 0) {
+                console.log("none\n")
+            } else {
+
+                console.log("Flavors list: \n" + arr + "\n");
+            }
+
+            break;
+
+        case 2: arr.push(readlineSync.question("flavor: "));
+                console.log("\n")
+            break;
+        case 3: let i = parseInt(readlineSync.question("position of flavor to delete: "));
+            arr.splice(i - 1, 1);
+
+            break;
+        case 4: process.exit();
+
+        default: console.log("not a valid command!\n");
+    }
+}
